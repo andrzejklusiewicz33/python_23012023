87280d51aac7bd92f5c919274534a81b570cae6f
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-01-23 11:00:29 +0100

    zadanie1

--- kod/main.py
+++ kod/main.py
@@ -1,2 +1,64 @@
-#test
-#test2
+# test
+# test2
+# print("siema!")
+# print('siema!')
+#
+# for x in range(10):
+#     print(f'x={x}')
+# print('koniec')
+
+#formatowanie kodu ctrl+alt+L
+#zakomentowywanie i odkomentowywanie ctrl+/
+
+# print("Siema!")
+# print('Siema!')
+# print("Mc'Donalds")
+#int x=1 #java
+#x=1
+# x=10
+# y=20
+# z=x*y
+# print(z)
+# print(x,y,z)
+# owoc="banan"
+# kolor="żółty"
+# cos='nietoperz'
+# print(owoc)
+# print("owoc="+owoc)
+# print("owoc={} kolor={}".format(owoc,kolor,cos))
+# owoc='banan'
+# print("owoc="+owoc)
+# print('owoc={}'.format(owoc))
+# print(f'owoc={owoc}')
+
+# owoc='banan'
+# pi=3.14
+# x=10
+# print(owoc,type(owoc))
+# print(pi,type(pi))
+# print(x,type(x))
+#
+# pi=3.14
+# print('pi='+pi)
+
+#
+# pi='3.14'
+# print('pi='+pi)
+# print(pi*10)
+# print("hajs"*1000)
+#
+# x=10
+# print('x='+str(x))
+# print('x={} x={}'.format(x,x))
+# print(f'x={x}')
+#
+# owoc=input('podaj swój ulubiony owoc:')
+# print(f'Twój ulubiony owoc to {owoc}')
+
+
+# owoc=input('podaj swój ulubiony owoc:\n')
+# print(f'Twój ulubiony owoc to {owoc}')
+
+
+#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+#wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
