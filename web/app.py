7eb960bb52206c7aae1645d2aaca531cbe56c8f7
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-01-26 15:06:42 +0100

    zadanie 52

--- web/app.py
+++ web/app.py
@@ -15,7 +15,9 @@ if __name__ == '__main__':
     app.run(debug=True,port=80)
 
 
-#52. 
+#52. Dodaj obsługę ekranów pod adresami /show_products /show_employees /about
+#Po wejściu na każdy z tych ekranów powinien pojawić się komunikat z tekstem jednoznacznie identyfikujacym na jakiej podstronie jesteściee
+
 
 #Flask,Django,FastApi, Pyramid
 #uWSGI
