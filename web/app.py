7c8bdcfb696f5e0a36f4368c25bae495a9791261
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-01-26 16:07:19 +0100

    zadanie 56

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from domain import *
 
 app = Flask(__name__)
 
@@ -17,14 +18,23 @@ def show_employees():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    fn="Andrzej"
+    ln="Klusiewicz"
+    em="klusiewicz@jsystems.pl"
+    return render_template("about.html",first_name=fn,last_name=ln,email=em)
+
+
 
 @app.route('/examples')
 def examples():
-    print("Ktoś wszedł na ekran /examples....")
-    f="banana"
-    c="yellow"
-    return render_template("examples.html",fruit=f,color=c)
+    return render_template("examples.html",fruit=Fruit())
+
+# @app.route('/examples')
+# def examples():
+#     print("Ktoś wszedł na ekran /examples....")
+#     f="banana"
+#     c="yellow"
+#     return render_template("examples.html",fruit=f,color=c)
 
 
 if __name__ == '__main__':
@@ -36,7 +46,9 @@ if __name__ == '__main__':
 #53. Zadbaj o to, by wszystkie ekrany były obsługiwane przez osobne pliki html
 #54. Do wszystkich ekranów dodaj działającce menu
 #55. Do widoku ekranu /about przekaż informację o swoim imieniu, nazwisku i emailu. Wyświetl te dane na ekranie w formie tabelki.
-
+#56. W osobnym module domain stwórz klasę Author z polami first_name,last_name, email (i ustawionymi w polach wartościami)
+#Następnie spowoduj by kontroler ekranu /about przekazywał do widoku obiekt tej klasy. Na poziomie widoku
+#zaktualizuj kod w taki sposób by wyświetlane były dane z przekazanego obiektu
 
 #Flask,Django,FastApi, Pyramid
 #uWSGI
---
+++ web/domain.py
@@ -0,0 +1,3 @@
+class Fruit:
+    name="Banana"
+    color="yellow"
