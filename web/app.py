412fa318eed84f2dc4d51e409b80ee4dce7d08e2
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-01-26 15:46:52 +0100

    zadanie 54

--- web/app.py
+++ web/app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Strona główna'
+    return render_template("index.html")
 
 @app.route('/show_products')
 def show_products():
-    return "<h1>Lista produktów</h1>"
+    return render_template("show_products.html")
 
 @app.route('/show_employees')
 def show_employees():
-    return "<h1>Lista pracowników</h1>"
+    return render_template("show_employees.html")
 
 @app.route('/about')
 def about():
-    return "<h1>Strona o programie</h1>"
+    return render_template("about.html")
 
 @app.route('/examples')
 def examples():
@@ -32,7 +32,10 @@ if __name__ == '__main__':
 #52. Dodaj obsługę ekranów pod adresami /show_products /show_employees /about
 #Po wejściu na każdy z tych ekranów powinien pojawić się komunikat z tekstem jednoznacznie identyfikujacym na jakiej podstronie jesteściee
 #53. Zadbaj o to, by wszystkie ekrany były obsługiwane przez osobne pliki html
+#54. Do wszystkich ekranów dodaj działającce menu
 
 #Flask,Django,FastApi, Pyramid
 #uWSGI
 #https://www.pythonanywhere.com/
+
+#przerwa do 15:36
